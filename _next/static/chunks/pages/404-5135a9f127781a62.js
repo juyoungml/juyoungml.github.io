fde0db4553c48373cc7837df088b6f131171bafa
juyoungml <scottsuk0306@gmail.com>
2025-08-16 09:51:17 +0000

    Deploying to gh-pages from @ juyoungml/juyoungml.github.io@92c2d4b6774ec70640ab57ae8d4cd7c8b684fb8c 🚀

---
+++ _next/static/chunks/pages/404-5135a9f127781a62.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{6141:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(3232)}])},3232:function(n,u,_){"use strict";_.r(u);var t=_(5893);u.default=()=>(0,t.jsx)("h1",{children:"404 - Page Not Found"})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=6141)}),_N_E=n.O()}]);
